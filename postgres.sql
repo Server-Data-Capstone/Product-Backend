d2e53b7d5358d771bb7ffe71745040c8d3065d57
Sahid Adin <sahidadin2@gmail.com>
2023-05-25 22:55:50 -0700

    added etl code

--- postgres.sql
+++ postgres.sql
@@ -1,59 +1,63 @@
-create database Products;
+DROP DATABASE IF EXISTS products;
+create database products;
 
-use Products;
+\c products;
 
-CREATE TABLE product {
-  id serial INT PRIMARY KEY,
-  product_id INT UNIQUE,
-  name VARCHAR ( 50 ) UNIQUE NOT NULL,
-  slogan VARCHAR ( 50 ) NOT NULL,
+CREATE TABLE product (
+  id serial  PRIMARY KEY,
+  name TEXT UNIQUE NOT NULL,
+  slogan TEXT NOT NULL,
   description TEXT NOT NULL,
   category TEXT NOT NULL,
   default_price TEXT NOT NULL
-}
+);
 
-CREATE TABLE features {
-  id serial INT PRIMARY KEY,
-  product_id INT UNIQUE,
+CREATE TABLE features (
+  id serial PRIMARY KEY,
+  product_id INTEGER REFERENCES product(id),
   feature TEXT,
   value TEXT
-}
+);
 
-CREATE TABLE styles {
-   id serial INT PRIMARY KEY,
-  productId INT UNIQUE,
+
+CREATE TABLE styles (
+   id serial PRIMARY KEY,
+  product_id INTEGER REFERENCES product(id),
   name VARCHAR ( 50 ) UNIQUE NOT NULL,
-  sale_price INT,
-  original_price INT NOT NULL,
+  sale_price INTEGER,
+  original_price INTEGER NOT NULL,
   default_style BOOLEAN
-}
+);
 
-CREATE TABLE photos {
-   id serial INT PRIMARY KEY,
-  styleId INT NOT NULL,
+CREATE TABLE photos (
+   id serial PRIMARY KEY,
+  styleId INTEGER NOT NULL,
   url TEXT NOT NULL,
-  thumbnail_url TEXT NOT NULL,
-}
+  thumbnail_url TEXT NOT NULL
+);
 
-CREATE TABLE skus {
-  id serial INT PRIMARY KEY,
-  styleId INT NOT NULL,
+CREATE TABLE skus (
+  id serial PRIMARY KEY,
+  styleId INTEGER NOT NULL,
   size TEXT NOT NULL,
-  quantity INT NOT NULL,
-}
+  quantity INTEGER NOT NULL
+);
 
-CREATE TABLE related {
-   id serial INT PRIMARY KEY,
-  current_product_id INT NOT NULL,
-  related_product_id INT NOT NULL,
+CREATE TABLE related (
+   id serial PRIMARY KEY,
+  current_product_id INTEGER REFERENCES product(id),
+  related_product_id INTEGER NOT NULL
 
-}
+);
 
-CREATE TABLE cart {
-id serial INT PRIMARY KEY,
-  user_session INT NOT NULL,
-  product_id INT NOT NULL,
-  active INT NOT NULL,
+COPY product FROM '/Users/sahidblapped/Desktop/hackreactor/Product-Backend/data/product.csv' DELIMITER ',' CSV HEADER;
 
+COPY features FROM '/Users/sahidblapped/Desktop/hackreactor/Product-Backend/data/features.csv' DELIMITER ',' CSV HEADER;
 
-}
+COPY styles FROM '/Users/sahidblapped/Desktop/hackreactor/Product-Backend/data/styles.csv' DELIMITER ',' CSV HEADER;
+
+COPY photos FROM '/Users/sahidblapped/Desktop/hackreactor/Product-Backend/data/photos.csv' DELIMITER ',' CSV HEADER;
+
+COPY skus FROM '/Users/sahidblapped/Desktop/hackreactor/Product-Backend/data/skus.csv' DELIMITER ',' CSV HEADER;
+
+COPY related FROM '/Users/sahidblapped/Desktop/hackreactor/Product-Backend/data/related.csv' DELIMITER ',' CSV HEADER;
